cc30a313edc13183450a63688239c63aee54dd23
estelp <pakyendouename@gmail.com>
2024-11-13 16:21:49 +0000

    add files to repository

--- Wrappers/mapping_pipeline.sh
+++ Wrappers/mapping_pipeline.sh
@@ -35,15 +35,15 @@ module load samtools/1.18
 
 # List of sequences
 sequences=("AG0004" "BN0123" "CH0461" "G22" "IE1K" "IR0015" "ML33" "PH42" "TN0057"
-       "Arcadia" "BN0202" "CH0533" "GFSI1-7-2" "IN0017" "IR0083" "NG0012" "PL2-1" "TN0065"
-       "B2" "BN0252" "CH1103" "GG11" "IN0054" "IR0084" "NG0054" "SSFL02" "TN0090"
-       "B71" "Br7" "CH1164" "GN0001" "IN0059" "IR0088" "NP0058" "SSFL14-3" "TR0025"
-       "Bd8401" "Br80" "CHRF" "GY0040" "IN0114" "IR0095" "P28" "T25" "US0041"
-       "BdBar" "CD0065" "CHW" "HO" "IN0115" "IT0010" "P29" "TG0004" "US0064"
-       "BF0072" "CD0142" "CM0028" "IA1" "IN0116" "JP0091" "P3" "TG0032" "VT0027"
-       "Bm88324" "CH0043" "FR1067" "IB33" "INA168" "LpKY-97-1" "Pg1213-22" "TN0001" "VT0030"
-       "BN0019" "CH0072" "FR1069" "IB49" "IR00102" "ML0060" "PgKY4OV2-1" "TN0002" "Z2-1"
-       "BN0119" "CH0452" "G17" "IC17" "IR0013" "ML0062" "PgPA18C-02" "TN0050")
+           "Arcadia" "BN0202" "CH0533" "GFSI1-7-2" "IN0017" "IR0083" "NG0012" "PL2-1" "TN0065"
+           "B2" "BN0252" "CH1103" "GG11" "IN0054" "IR0084" "NG0054" "SSFL02" "TN0090"
+           "B71" "Br7" "CH1164" "GN0001" "IN0059" "IR0088" "NP0058" "SSFL14-3" "TR0025"
+           "Bd8401" "Br80" "CHRF" "GY0040" "IN0114" "IR0095" "P28" "T25" "US0041"
+           "BdBar" "CD0065" "CHW" "HO" "IN0115" "IT0010" "P29" "TG0004" "US0064"
+           "BF0072" "CD0142" "CM0028" "IA1" "IN0116" "JP0091" "P3" "TG0032" "VT0027"
+           "Bm88324" "CH0043" "FR1067" "IB33" "INA168" "LpKY-97-1" "Pg1213-22" "TN0001" "VT0030"
+           "BN0019" "CH0072" "FR1069" "IB49" "IR00102" "ML0060" "PgKY4OV2-1" "TN0002" "Z2-1"
+           "BN0119" "CH0452" "G17" "IC17" "IR0013" "ML0062" "PgPA18C-02" "TN0050")
 
 #################################################
 
@@ -54,8 +54,8 @@ mkdir -p "$SAM_PATH" "$BAM_PATH" "$STATS_PATH" "$FILTERED_PATH" "$SORTED_PATH"
 
 ########### Step 1: Mapping, SAM to BAM conversion, Statistics, Filtering, Sorting ##################
 
-# Create the CSV statistics file
-echo "sample,mapped,primary_mapped,properly_paired,unmapped" > "$STAT_FILE"
+# Create the CSV statistics file and add headers
+echo "sample,total_reads,total_reads_perc,mapped_reads,mapped_reads_perc,properly_paired,properly_paired_perc,singletons,singletons_perc,unmapped_reads,unmapped_reads_perc,read1,read1_perc,read2,read2_perc" > "$STAT_FILE"
 
 # Loop over each sequence to perform all steps
 for sequence in "${sequences[@]}"; do
@@ -82,13 +82,6 @@ for sequence in "${sequences[@]}"; do
     samtools flagstat -@ 8 "$BAM_FILE" > "$FLAGSTAT_FILE"
     echo "Statistics generated for ${sequence}: ${FLAGSTAT_FILE}"
 
-    # Extract statistics data and append it to the CSV file
-    mapped=$(grep "mapped (" "$FLAGSTAT_FILE" | awk '{print $1}')
-    primary_mapped=$(grep "primary mapped (" "$FLAGSTAT_FILE" | awk '{print $1}')
-    properly_paired=$(grep "properly paired (" "$FLAGSTAT_FILE" | awk '{print $1}')
-    unmapped=$(grep "unmapped (" "$FLAGSTAT_FILE" | awk '{print $1}')
-    echo "${sequence},${mapped},${primary_mapped},${properly_paired},${unmapped}" >> "$STAT_FILE"
-    
     # Step 4: Filter BAM files
     samtools view -bh -@ 8 -f 0x02 -o "$FILTERED_BAM" "$BAM_FILE"
     echo "Filtered BAM created for ${sequence}: ${FILTERED_BAM}"
@@ -101,4 +94,36 @@ for sequence in "${sequences[@]}"; do
     samtools index "$SORTED_BAM"
     echo "Indexing completed for ${sequence}"
     
+done
+
+# Process each flagstat file and write results to CSV with percentages
+for file in "$STATS_PATH"/*flagstat; do
+    if [[ -f $file ]]; then
+        sample_name=$(basename "$file" | cut -d. -f1)
+        new_line="$sample_name,"
+        
+        # Extract values from the flagstat file
+        total_reads=$(grep "in total" "$file" | awk '{print $1}')
+        mapped_reads=$(grep "mapped (" "$file" | awk '{print $1}')
+        mapped_reads_perc=$(grep "mapped (" "$file" | awk -F '[()%]' '{print $2}')
+        
+        properly_paired=$(grep "properly paired (" "$file" | awk '{print $1}')
+        properly_paired_perc=$(grep "properly paired (" "$file" | awk -F '[()%]' '{print $2}')
+        
+        singletons=$(grep "singletons (" "$file" | awk '{print $1}')
+        singletons_perc=$(grep "singletons (" "$file" | awk -F '[()%]' '{print $2}')
+        
+        unmapped_reads=$((total_reads - mapped_reads))
+        unmapped_reads_perc=$(awk "BEGIN {print (100 - $mapped_reads_perc)}")
+        
+        read1=$(grep "read1" "$file" | awk '{print $1}')
+        read1_perc=$(awk "BEGIN {print ($read1/$total_reads)*100}")
+        
+        read2=$(grep "read2" "$file" | awk '{print $1}')
+        read2_perc=$(awk "BEGIN {print ($read2/$total_reads)*100}")
+        
+        # Append statistics with percentages to the CSV file
+        new_line+="${total_reads},100,${mapped_reads},${mapped_reads_perc},${properly_paired},${properly_paired_perc},${singletons},${singletons_perc},${unmapped_reads},${unmapped_reads_perc},${read1},${read1_perc},${read2},${read2_perc}"
+        echo "$new_line" >> "$STAT_FILE"
+    fi
 done
