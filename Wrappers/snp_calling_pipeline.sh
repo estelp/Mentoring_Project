cb98c2e29ff5d4253aa6660e56b78032c57984db
estelp <pakyendouename@gmail.com>
2024-11-15 13:46:21 +0000

    correction of some informations in snp_calling script

--- Wrappers/snp_calling_pipeline.sh
+++ Wrappers/snp_calling_pipeline.sh
@@ -11,6 +11,9 @@
 ### Define number of CPUs to use
 #SBATCH -c 16
 
+### Specify the node to run on
+#SBATCH --nodelist=node20  # Spécifie que le job doit être exécuté sur node20
+
 #################################################
 
 ########### Execution Command ##################
@@ -23,6 +26,7 @@ REF_GENOME="/scratch/MOryzae/REF/MOryzae_genomic.fna"
 SNP_STATS_DIR="/scratch/MOryzae/SNP/stats"
 OUTPUT_VCF="${VCF_PATH}.gz"
 SNP_FILE="/scratch/MOryzae/SNP/vcf_files/all_samples_snp.vcf"
+OUTPUT_VCF2="${SNP_FILE}.gz"
 ALLELE_FREQ_PATH="/scratch/MOryzae/SNP/allele_frequence"
 
 # Load necessary modules
@@ -33,34 +37,32 @@ module load vcftools/0.1.16
 # Create directories if necessary
 mkdir -p /scratch/MOryzae/SNP/bcf_files /scratch/MOryzae/SNP/vcf_files "$SNP_STATS_DIR" "$ALLELE_FREQ_PATH"
 
-# Check for the existence of sorted BAM files
-if ls "$SORTED_PATH"/*.mappedpaired.sorted.bam 1> /dev/null 2>&1; then
-    # Use bcftools for mpileup and variant calling
-    echo -e "######################\nGenerating BCF file"
-    bcftools mpileup --threads 16 -f "$REF_GENOME" -O b -o "$BCF_PATH" "$SORTED_PATH"/*.mappedpaired.sorted.bam
 
-    echo -e "######################\nVariant calling"
-    bcftools call --threads 16 -v -c -o "$VCF_PATH" "$BCF_PATH"
+# Use bcftools for mpileup and variant calling
+echo -e "######################\nGenerating BCF file"
+bcftools mpileup --threads 16 -f "$REF_GENOME" -O b -o "$BCF_PATH" "$SORTED_PATH"/*.mappedpaired.sorted.bam
 
-    echo -e "######################\nGenerating SNP statistics"
-    bcftools stats "$VCF_PATH" > "$SNP_STATS_DIR/all_samples_SNP_statistics.txt"
+echo -e "######################\nVariant calling"
+bcftools call --threads 16 -v -c -o "$VCF_PATH" "$BCF_PATH"
 
-    # Filter to keep only SNPs
-    echo -e "######################\nFiltering SNPs"
-    bcftools view -v snps "$VCF_PATH" -o "$SNP_FILE"
+echo -e "######################\nGenerating SNP statistics"
+bcftools stats "$VCF_PATH" > "$SNP_STATS_DIR/all_samples_SNP_statistics.txt"
+# Filter to keep only SNPs
+echo -e "######################\nFiltering SNPs"
+bcftools view -v snps "$VCF_PATH" -o "$SNP_FILE"
 
-    # Compress and index the VCF file
-    echo -e "######################\nCompressing and indexing VCF file"
-    bgzip -c "$SNP_FILE" > "$OUTPUT_VCF"
-    bcftools index "$OUTPUT_VCF"
+# Compress and index the VCF file
+echo -e "######################\nCompressing and indexing VCF file"
+bgzip -c "$SNP_FILE" > "$OUTPUT_VCF2"
+bcftools index "$OUTPUT_VCF"
+bcftools index "$OUTPUT_VCF2"
 
-    # Calculate allele frequencies
-    echo -e "######################\nCalculating allele frequencies"
-    vcftools --gzvcf "$OUTPUT_VCF" --freq --out "${ALLELE_FREQ_PATH}/AF" --max-alleles 2
-    vcftools --gzvcf "$OUTPUT_VCF" --freq2 --out "${ALLELE_FREQ_PATH}/AF2" --max-alleles 2
-    
-    echo "BCF and VCF files generated successfully."
-else
-    echo "No sorted BAM files found in $SORTED_PATH. Please check the path and try again."
-    exit 1
-fi
+# Calculate allele frequencies
+echo -e "######################\nCalculating allele frequencies"
+vcftools --gzvcf "$OUTPUT_VCF" --freq --out "${ALLELE_FREQ_PATH}/AF" --max-alleles 2
+vcftools --gzvcf "$OUTPUT_VCF" --freq2 --out "${ALLELE_FREQ_PATH}/AF2" --max-alleles 2
+
+vcftools --gzvcf "$OUTPUT_VCF2" --freq --out "${ALLELE_FREQ_PATH}/AF_2" --max-alleles 2
+vcftools --gzvcf "$OUTPUT_VCF2" --freq2 --out "${ALLELE_FREQ_PATH}/AF2_2" --max-alleles 2
+
+echo "BCF and VCF files generated successfully."
